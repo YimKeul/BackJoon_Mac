fcb8e354dd1001aa5239da8ee3052d265f8b946f
yimkeul <leesjun29@gmail.com>
2022-06-27 17:25:55 +0900

    add

--- Q_2110/공유기설치.py
+++ Q_2110/공유기설치.py
@@ -7,28 +7,52 @@ for i in range(n):
 array.sort()
 
 
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0]
-        count = 1
+# def binary_search(array, start, end):
+#     while start <= end:
+#         mid = (start + end) // 2
+#         current = array[0]
+#         count = 1
 
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
+#         for i in range(1, len(array)):
+#             if array[i] >= current + mid:
+#                 count += 1
+#                 current = array[i]
 
-        if count >= c:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
+#         if count >= c:
+#             global answer
+#             start = mid + 1
+#             answer = mid
+#         else:
+#             end = mid - 1
 
 
-start = 1
-end = array[-1] - array[0]
-answer = 0
+# start = 1
+# end = array[-1] - array[0]
+# answer = 0
 
-binary_search(array, start, end)
-print(answer)
+# binary_search(array, start, end)
+# print(answer)
+
+
+min = 1
+max = array[-1] - array[0]
+result = 0
+
+while (max>=min):
+    mid = (max+min)//2
+    count = 1
+    current = array[0]
+
+    for i in range(1,len(array)):
+        if array[i] >= current+mid:
+            count+=1
+            current = array[i]
+    
+    if count>=c:
+        result = mid
+        min = mid+1
+    else:
+        max = mid-1
+
+print(result)
+        
